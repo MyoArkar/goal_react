fd62261bfb1279456baab1293be69bd9a2bcffdf
Myo Arkar <myoarkar430@gmail.com>
2025-01-18 00:17:14 +0630

    GoalCRUD by MyoArkar

--- src/views/Goal/GoalDetail.jsx
+++ src/views/Goal/GoalDetail.jsx
@@ -9,72 +9,73 @@ export default function GoalDetail() {
 
   const fetchGoal = async () => {
     try {
-        const { data } = await axiosClient.get(`/goals/${id}`);
-        setGoal(data.data);
+      const { data } = await axiosClient.get(`/goals/${id}`);
+      setGoal(data.data);
       console.log(data.message);
       console.log(data)
     } catch (error) {
-        console.error("Error fetching goal:", error);
-        alert("Failed to fetch goal. Please try again.");
+      console.error("Error fetching goal:", error);
+      alert("Failed to fetch goal. Please try again.");
     }
   };
   useEffect(() => {
-  
-  
-          fetchGoal();
-      }, []);
+
+
+    fetchGoal();
+  }, []);
   return <>
-<div className="container text-gray-400 px-10 py-10">
-  <div className="bg-gray-900 px-10 py-3 rounded">
-    <div className="bg-gray-800 px-4 py-3 rounded-t-md">
-          <h2 className="text-gray-200 font-semibold">{goal.title}</h2>
-    </div>
-    <div className="mt-4 flex justify-between items-center">
-      <div>
-        <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded-full text-xs mr-3">
-          {goal.priority} Priority
-        </span>
-        <span className="bg-gray-800 text-gray-400 px-2 py-1 rounded-full text-xs">
-          {goal.status}
-        </span>
-      </div>
-      <div className="flex justify-between items-center gap-3">
-        <h3 className="text-gray-300 font-medium">Progress:</h3>
-        <div className="progressbar relative w-48 h-2 rounded-lg bg-gray-700">
-          <span className="absolute top-0 left-0 w-24 h-2 bg-gray-500 rounded-lg"></span>
+    <div className="container text-[#0e0e0e] px-10 py-10">
+      <div className="border-[#0e0e0e] border-2 rounded-3xl overflow-hidden">
+        <div className="bg-[#0e0e0e] text-slate-50 px-4 py-3">
+          <h2 className="font-semibold">{goal.title}</h2>
         </div>
-        <p className="text-gray-300">40%</p>
-      </div>
-    </div>
-    <div className="grid grid-cols-2 gap-2">
-      <div className="mt-3 bg-gray-800 px-4 py-3 h-[150px] rounded-md">
-        <h3 className="text-gray-200 font-medium">Description</h3>
-        <p className="h-[100px] overflow-scroll no-scrollbar text-gray-300">
-          {goal.description}
-        </p>
-      </div>
-      <div className="mt-3 bg-gray-800 px-4 py-3 h-[150px] rounded-md">
-        <h3 className="text-gray-200 font-medium">Timeline</h3>
-        <div className="flex items-center justify-between my-3">
-          <div className="flex items-center gap-3 text-gray-300">
-            <ion-icon size="large" name="calendar-outline"></ion-icon>
-            <h4>Start Date:</h4>
+        <div className='py-4 px-3'>
+          <div className="flex justify-between items-center">
+            <div>
+              <span className="border-[#0e0e0e] text-[#0e0e0e] px-2 py-1 rounded-full text-xs mr-3 border-2">
+                {goal.priority} Priority
+              </span>
+              <span className="border-[#0e0e0e] text-[#0e0e0e] px-2 py-1 rounded-full text-xs border-2">
+                {goal.status}
+              </span>
+            </div>
+            <div className="flex justify-between items-center gap-3">
+              <h3 className="text-[#0e0e0e] font-medium">Progress:</h3>
+              <div className="progressbar relative w-48 h-2 rounded-lg border-[#0e0e0e] border-2">
+                <span className="absolute top-0 left-0 w-24 h-2 bg-[#0e0e0e] rounded-lg"></span>
+              </div>
+              <p className="text-[#0e0e0e]">40%</p>
+            </div>
           </div>
-              <p className="text-gray-400">{formatDate(goal.start_date)}</p>
-        </div>
-        <div className="flex items-center justify-between my-3">
-          <div className="flex items-center gap-3 text-gray-300">
-            <ion-icon size="large" name="flag-outline"></ion-icon>
-            <h4>End Date:</h4>
+          <div className="grid grid-cols-2 gap-2">
+            <div className="mt-3 border-[#0e0e0e] px-4 py-3 h-[150px] rounded-md border-2">
+              <h3 className="text-slate-50 font-medium">Description</h3>
+              <p className="h-[100px] overflow-scroll no-scrollbar text-[#0e0e0e]">
+                {goal.description}
+              </p>
+            </div>
+            <div className="mt-3 border-[#0e0e0e] px-4 py-3 h-[150px] rounded-md border-2">
+              <h3 className="text-slate-50 font-medium">Timeline</h3>
+              <div className="flex items-center justify-between my-3">
+                <div className="flex items-center gap-3 text-[#0e0e0e]">
+                  <ion-icon size="large" name="calendar-outline"></ion-icon>
+                  <h4>Start Date:</h4>
+                </div>
+                <p>{formatDate(goal.start_date)}</p>
+              </div>
+              <div className="flex items-center justify-between my-3">
+                <div className="flex items-center gap-3 text-[#0e0e0e]">
+                  <ion-icon size="large" name="flag-outline"></ion-icon>
+                  <h4>End Date:</h4>
+                </div>
+                <p>{formatDate(goal.end_date)}</p>
+              </div>
+            </div>
           </div>
-              <p className="text-gray-400">{formatDate(goal.end_date)}</p>
         </div>
+
       </div>
     </div>
-  </div>
-</div>
-
-
-
   </>
+
 }
--- src/views/Goal/Model.jsx
+++ src/views/Goal/Model.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { convertToISOString } from '../../utilities/dateFormater';
+import axiosClient from '../../axiosClient';
+import { convertToDateTime } from '../../utilities/dateFormater';
 
-export default function Model({ visible, onClose }) {
+export default function Model({ visible, onClose, update, goal }) {
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+    const startRef = useRef();
+    const endRef = useRef();
+    const priorityRef = useRef();
+    const statusRef = useRef();
+
+    useEffect(() => {
+        if (update && goal) {
+            titleRef.current.value = goal.title;
+            descriptionRef.current.value = goal.description;
+            startRef.current.value = convertToDateTime(goal.start_date); 
+            endRef.current.value = convertToDateTime(goal.end_date);
+            priorityRef.current.value = goal.priority;
+            statusRef.current.value = goal.status;
+        }
+    }, [update, goal]);
 
     if (!visible) return null;
+
+    const handleCreate = () => {
+        const payload = {
+            title: titleRef.current.value,
+            description: descriptionRef.current.value,
+            start_date: startRef.current.value,
+            end_date: endRef.current.value,
+            priority: priorityRef.current.value,
+            status: statusRef.current.value,
+        };
+
+        axiosClient.post("/goals", payload).then(() => {
+            onClose();
+        }).catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                console.log(response.data.errors);
+            }
+        });
+        console.log(payload)
+    };
+
+    const handleUpdate = () => {
+        const payload = {
+            title: titleRef.current.value,
+            description: descriptionRef.current.value,
+            start_date: convertToISOString(startRef.current.value),
+            end_date: convertToISOString(endRef.current.value),
+            priority: priorityRef.current.value,
+            status: statusRef.current.value,
+        };
+
+        axiosClient.put(`/goals/${goal.id}`, payload).then(() => {
+            onClose();
+        }).catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                console.log(response.data.errors);
+            }
+        });
+    };
+
     return (
         <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-10">
             <div className="w-[30rem] flex flex-col justify-center items-center text-slate-300 bg-[#0e0e0e] p-8 rounded-sm gap-5">
@@ -11,46 +73,61 @@ export default function Model({ visible, onClose }) {
                         <ion-icon size="large" name="close-circle-outline"></ion-icon>
                     </button>
                 </div>
-                <h3>Create New Goal</h3>
+                <h3>{update ? 'Update Goal' : 'Create New Goal'}</h3>
                 <div className='w-full flex flex-col'>
                     <label htmlFor="">Title</label>
-                    <input type="text" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
+                    <input  ref={titleRef} type="text" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
                 </div>
                 <div className='w-full flex flex-col'>
                     <label htmlFor="">Description</label>
-                    <input type="text" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
+                    <input ref={descriptionRef} type="text" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
                 </div>
                 <div className="w-full flex flex-row justify-between gap-3">
                     <div className='w-1/2 flex flex-col'>
                         <label htmlFor="">Start Date</label>
-                        <input type="date" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
+                        <input ref={startRef} type="datetime-local" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
                     </div>
                     <div className='w-1/2 flex flex-col'>
                         <label htmlFor="">End Date</label>
-                        <input type="date" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
+                        <input ref={endRef} type="datetime-local" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm' />
                     </div>
                 </div>
                 <div className="w-full flex flex-row justify-between gap-3">
                     <div className='w-1/2 flex flex-col'>
                         <label htmlFor="">Priority</label>
-                        <select name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm'>
-                            <option value="" selected>High</option>
-                            <option value="">Medium</option>
-                            <option value="">Low</option>
+                        <select ref={priorityRef} defaultValue="high" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm'>
+                            <option value="high" selected>High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
                         </select>
                     </div>
                     <div className='w-1/2 flex flex-col'>
                         <label htmlFor="">Status</label>
-                        <select name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm'>
-                            <option value="" selected>Complete</option>
-                            <option value="">In Process</option>
-                            <option value="">Pending</option>
+                        <select ref={statusRef} defaultValue="completed" name="" id="" className='outline-none p-2 bg-gray-800 rounded-sm'>
+                            <option value="completed" selected>Complete</option>
+                            <option value="in progress">In Process</option>
+                            <option value="in progress">Pending</option>
                         </select>
                     </div>
                 </div>
                 <div className="w-full flex flex-row-reverse gap-3">
                     <button className='bg-gray-800 px-3 py-2 rounded'>Cancel</button>
-                    <button className='bg-sky-500 px-3 py-2 rounded-sm text-white hover:bg-sky-600' type='submit'>Create Goal</button>
+                    
+                    {update ? (
+                        <button
+                            className='bg-sky-500 px-3 py-2 rounded-sm text-white hover:bg-sky-600'
+                            onClick={handleUpdate} // Ensure this function is defined for updating
+                        >
+                            Update Goal
+                        </button>
+                    ) : (
+                        <button
+                            className='bg-sky-500 px-3 py-2 rounded-sm text-white hover:bg-sky-600'
+                            onClick={handleCreate} // Ensure this function is defined for creating
+                        >
+                            Create Goal
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
