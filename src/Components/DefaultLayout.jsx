5685a9974b274d3b06db1c56d28da16fb53e5349
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-01-17 14:08:50 +0630

    dry commit

--- src/Components/DefaultLayout.jsx
+++ src/Components/DefaultLayout.jsx
@@ -1,25 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/contextprovider";
-import Sidebar from "./SideBar";
+import { Navigate } from "react-router-dom";
+import axiosClient from "../axiosClient";
 import Menu from "./Menu";
 
 export default function DefaultLayout() {
   const { user, token, setUser, setToken } = useStateContext();
-
-   if (!token) {
-     return <Navigate to="/login" />;
-   }
-   return (
-    <main className="flex h-screen overflow-hidden">
-      
-      <aside className="fixed h-full w-64 bg-gray-800 shadow-lg">
-        <Sidebar />
-      </aside>
-      
-      <section className="ml-64 flex-1 overflow-y-auto">
-        <Outlet />
-      </section>
-
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <main className="flex">
+      <Menu />
     </main>
   );
 }
