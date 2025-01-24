6494ff151089012e5c57af6c0f81e47c66ae8395
yzarzar <zar31528@gmail.com>
2025-01-24 11:07:05 +0630

    🎨✨ Created a stunning User Profile UI - Designed an elegant and user-friendly profile layout. - Enhanced visibility for the profile picture. - Improved overall aesthetics for desktop view.

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -28,13 +28,13 @@ export default function Login() {
   }
   
   return (
-    <div className="min-h-screen bg-gray-900 flex items-start justify-center">
-      <div className="mt-10 rounded-lg shadow-lg w-full max-w-sm">
-        <h1 className="text-2xl font-bold text-white text-center mb-8">
+    <div className="flex justify-center items-start min-h-screen bg-gray-900">
+      <div className="mt-10 w-full max-w-sm rounded-lg shadow-lg">
+        <h1 className="mb-8 text-2xl font-bold text-center text-white">
           Log in
         </h1>
         {error && (
-          <div className="mb-4 p-3 bg-red-500 text-white rounded-lg text-center">
+          <div className="p-3 mb-4 text-center text-white bg-red-500 rounded-lg">
             {error}
           </div>
         )}
@@ -44,33 +44,33 @@ export default function Login() {
               ref={emailRef}
               type="email"
               placeholder="Enter Your Email"
-              className="text-xl w-full px-4 py-3 bg-gray-800 text-white placeholder:text-gray-50 border-2 border-gray-600 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="px-4 py-3 w-full text-xl text-white bg-gray-800 rounded-xl border-2 border-gray-600 placeholder:text-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
             <input 
               ref={passwordRef}
               type="password"
               placeholder="Password"
-              className="text-xl w-full px-4 py-3 bg-gray-800 text-white placeholder:text-gray-50 border-2 border-gray-600 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="px-4 py-3 w-full text-xl text-white bg-gray-800 rounded-xl border-2 border-gray-600 placeholder:text-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
           <button
             type="submit"
-            className="text-xl font-bold w-full bg-sky-500 text-white py-4 px-4 rounded-xl border-b-4 border-sky-700 hover:bg-sky-400 transition-colors duration-300"
+            className="px-4 py-4 w-full text-xl font-bold text-white bg-sky-500 rounded-xl border-b-4 border-sky-700 transition-colors duration-300 hover:bg-sky-400"
           >
             Login
           </button>
         </form>
-        <div className="mt-4 text-gray-600 text-xl font-bold">
+        <div className="mt-4 text-xl font-bold text-gray-600">
           <p className="relative text-center before:content-[''] before:block before:w-[45%] before:h-1 before:rounded-full before:bg-gray-600 before:absolute before:left-0 before:top-1/2 after:content-[''] after:block after:w-[45%] after:h-1 after:rounded-full after:bg-gray-600 after:absolute after:right-0 after:top-1/2">
             OR
           </p>
         </div>
-        <button className="text-xl font-bold w-full text-sky-500 py-4 px-4 rounded-xl border-2 border-b-4 border-gray-600 transition-colors duration-300 mt-4">
+        <button className="px-4 py-4 mt-4 w-full text-xl font-bold text-sky-500 rounded-xl border-2 border-b-4 border-gray-600 transition-colors duration-300">
           <Link
             to="/register"
-            className="text-blue-400 hover:text-blue-500 transition-colors duration-300"
+            className="text-blue-400 transition-colors duration-300 hover:text-blue-500"
           >
             Sign Up
           </Link>
